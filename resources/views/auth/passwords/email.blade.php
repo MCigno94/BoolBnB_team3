7c12f79de62cf51f006360044951f50e8c07c518
ariannagreco3393 <a.greco3393@gmail.com>
2022-07-15 17:27:29 +0200

    add reset layout

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,45 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+<div class="container ct-form bg-white bg-md-gray">
+    <div class="row row-cols-1 row-cols-md-2 h-100 align-items-center justify-content-center">
+        <div class="col d-none d-md-block">
+            <div class="logo text-center">
+                <div>
+                    <img src="{{asset('img/logo.png')}}" alt="">
                 </div>
+                <div class="logo-text display-3 text-danger">
+                    BoolBnB
+                </div>
+            </div>
+        </div>
+        <div class="form col bg-danger d-flex align-items-center justify-content-center">
+            <div class="text-white">
+                <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <h2 class="display-4 pb-4">Reset your password</h2>
+                    <div class="form-group row">
+                        <label for="email" class="col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                        <div>
+                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+                            @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group row mb-0 pt-5">
+                        <div class="d-flex justify-content-center ">
+                            <button type="submit" class="btn btn-light fw-bold text-danger w-50">
+                                {{ __('Send Password Reset Link') }}
+                            </button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
